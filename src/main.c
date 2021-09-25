7e4c27b0963b6165a765efa80bdfdf347e2b9531
Siddharth Roy <siddharthroy36912@gmail.com>
2021-09-25 12:37:55 +0530

    Done

--- src/main.c
+++ src/main.c
@@ -1,37 +1,91 @@
-#include "raylib.h"
+/*******************************************************************************************
+*
+*   raylib [core] example - Initialize 3d camera mode
+*
+*   This example has been created using raylib 1.0 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+#include "rlgl.h"
+#include <stdio.h>
 
 #ifndef ASSETS_PATH
 #define ASSETS_PATH
 #endif
 
+#include "raytile.h"
+#include "raylib.h"
+
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-    SetTargetFPS(60);
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera mode");
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
-        ClearBackground(RAYWHITE);
+    SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
 
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+    #define SIZE_X 500
+    #define SIZE_Y 500
+    int texCoords[SIZE_X][SIZE_Y][2];
 
-        EndDrawing();
+    for (int x = 0; x < SIZE_X; x++) {
+        for (int y = 0; y < SIZE_Y; y++) {
+            texCoords[x][y][0] = GetRandomValue(0, 1);
+            texCoords[x][y][1] = 0;
+        }
     }
 
-    CloseWindow();
+    Texture2D texture = LoadTexture(ASSETS_PATH"tiles.png");    // Load map texture
+    Model testModel = rtl_gen_tile_model(1, SIZE_X, SIZE_Y,  8, 8, texCoords, texture);
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        UpdateCamera(&camera);
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawModel(testModel, cubePosition, 1, WHITE);
+
+            EndMode3D();
+
+            DrawText("Welcome to the third dimension!", 10, 40, 20, DARKGRAY);
+
+            DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadModel(testModel);
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
     return 0;
 }
